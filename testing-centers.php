45282cf00f899378306e8739c3f4a74763162ef7
Rocio Cardona <rocio.cardona03@gmail.com>
2020-07-12 15:37:52 -0600

    Created testing-centers.php Page shows nearest test centers to campus

---
+++ testing-centers.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+<head>
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="assets/img/fav.png">
+    <!-- Author Meta -->
+    <meta name="author" content="colorlib">
+    <!-- Meta Description -->
+    <meta name="description" content="">
+    <!-- Meta Keyword -->
+    <meta name="keywords" content="">
+    <!-- meta character set -->
+    <meta charset="UTF-8">
+    <!-- Site Title -->
+    <title>Testing Centers</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+    <!--
+    CSS
+    ============================================= -->
+    <link rel="stylesheet" href="assets/css/linearicons.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+    <link rel="stylesheet" href="assets/css/jquery-ui.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <style>
+        iframe {
+            border: none;
+            width: 100%;
+            height: 500px;
+        }
+
+        @media (min-width: 1024px) {
+            .content_viewport {
+                border: 0px none;
+                height: 900px;
+                width: 100%;
+            }
+        }
+
+        @media (max-width: 1023px) {
+            .content_viewport {
+                border: 0px none;
+                height: 900px;
+                width: 100%;
+            }
+        }
+
+        @media (min-width: 768px) {
+            .content_viewport {
+                border: 0px none;
+                height: 900px;
+                width: 100%;
+            }
+        }
+
+        .req {
+            color: #0c5460;
+            font-size: 14px;
+        }
+
+        .center {
+            text-align: center;
+        }
+
+    </style>
+</head>
+<body>
+<header id="header" id="home">
+    <?php require_once "navbar.php"; ?>
+</header><!-- #header -->
+
+<!-- start banner Area -->
+<section class="banner-area relative about-banner" id="home">
+    <div class="overlay overlay-bg"></div>
+    <div class="container">
+        <div class="row d-flex align-items-center justify-content-center">
+            <div class="about-content col-lg-12">
+                <!--                <h1 class="text-white">-->
+                <!--                    Testing Centers-->
+                <!--                </h1>-->
+                <!--                <p class="text-white link-nav"><a href="index.php">Home </a> <span class="lnr lnr-arrow-right"></span> <a href="testing-centers.php"> Popular Courses</a></p>-->
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End banner Area -->
+
+<!-- Start popular-courses Area -->
+<section class="popular-courses-area section-gap courses-page">
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="menu-content pb-70 col-lg-8">
+                <div class="title text-center">
+                    <h1 class="mb-10">Testing Centers near campus</h1>
+                    <p>Find the nearest testing centers below:</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="single-popular-carusel col-lg-6 col-md-7">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3095.158633942006!2d-86.53412878434338!3d39.12562147953611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886c66484f955555%3A0xecf37ddbf3b94e47!2sLogistics%20Health%20COVID-19%20Test%20Site!5e0!3m2!1sen!2sus!4v1594586502417!5m2!1sen!2sus"
+                                width="600" height="450" frameborder="0" style="border:0;" allowfullscreen aria-hidden="false" tabindex="0"></iframe>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="https://goo.gl/maps/6K9DxjGyPf5XtpJ27" target="_blank">
+                        <h4>Logistics Health COVID-19 Test Site <br><span class="req">Medical Laboratory</span></h4>
+                    </a>
+                    <p>
+                        <i class="fa fa-map-marker" aria-hidden="true"></i> Bloomington, IN<br>
+                        <i class="fa fa-exclamation" style="color: #CC0000" aria-hidden="true"></i> &nbsp;&nbsp;Appointment required<br>
+                        <i class="fa fa-check" style="color: green" aria-hidden="true"></i> Referral not required<br>
+                        <i class="fa fa-check" style="color: green" aria-hidden="true"></i> Testing for all patients <br>
+                        <i class="fa fa-phone info" style="color: #31708f" aria-hidden="true"></i> Call: 777-777-7777
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-6 col-md-7">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3094.7584014308054!2d-86.57169368419649!3d39.134728879534705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886c679aaed8980b%3A0xdc751a812c61dae7!2sSummit%20Urology!5e0!3m2!1sen!2sus!4v1594587535555!5m2!1sen!2sus"
+                                width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div>
+                </div>
+                <div class="details">
+                    <a href="https://goo.gl/maps/Yt8B6rQt3HrtZ1ZLA" target="_blank">
+                        <h4>Summit Urology<br><span class="req">Urologist</span></h4></a>
+                    <p>
+                        <i class="fa fa-map-marker" aria-hidden="true"></i> Bloomington, IN<br>
+                        <i class="fa fa-exclamation" style="color: #CC0000" aria-hidden="true"></i> &nbsp;&nbsp;Appointment required<br>
+                        <i class="fa fa-check" style="color: green" aria-hidden="true"></i> Referral not required<br>
+                        <i class="fa fa-exclamation" style="color: #CC0000" aria-hidden="true"></i> &nbsp;&nbsp;Tests limited to certain patients<br>
+                        <i class="fa fa-phone info" style="color: #31708f" aria-hidden="true"></i> &nbsp;Call: 777-777-7777
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-6 col-md-7">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3093.370913609003!2d-86.53633828419571!3d39.166287779530144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886c66ddeeeaaaab%3A0xe0b638df5ca6b8f9!2sFletcher%20Sport%20And%20Family%20Health!5e0!3m2!1sen!2sus!4v1594588180880!5m2!1sen!2sus"
+                                width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div>
+                </div>
+                <div class="details"><a href="https://goo.gl/maps/ZLPV13oi1pSWdiSu8" target="_blank">
+                        <h4>Fletcher Sport And Family Health<br><span class="req">Medical clinic</span></h4></a>
+                    <p>
+                        <i class="fa fa-map-marker" aria-hidden="true"></i> Bloomington, IN<br>
+                        <i class="fa fa-exclamation" style="color: #CC0000" aria-hidden="true"></i> &nbsp;&nbsp;Appointment required<br>
+                        <i class="fa fa-check" style="color: green" aria-hidden="true"></i> Referral not required<br>
+                        <i class="fa fa-exclamation" style="color: #CC0000" aria-hidden="true"></i> &nbsp;&nbsp;Tests limited to certain patients<br>
+                        <i class="fa fa-phone info" style="color: #31708f" aria-hidden="true"></i> &nbsp;Call: 777-777-7777
+                    </p>
+                </div>
+            </div>
+            <div class="single-popular-carusel col-lg-6 col-md-7">
+                <div class="thumb-wrap relative">
+                    <div class="thumb relative">
+                        <div class="overlay overlay-bg"></div>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3093.370913609003!2d-86.53633828419571!3d39.166287779530144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886c66ddeeeaaaab%3A0xe0b638df5ca6b8f9!2sFletcher%20Sport%20And%20Family%20Health!5e0!3m2!1sen!2sus!4v1594588180880!5m2!1sen!2sus"
+                                width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div>
+                </div>
+                <div class="details"><a href="https://goo.gl/maps/Ee5YKLbhYg1ZGJvb9" target="_blank">
+                        <h4>IU Health Bloomington Remote Specimen Collection<br><span class="req">Medical laboratory</span></h4></a>
+                    <p>
+                        <i class="fa fa-map-marker" aria-hidden="true"></i> Bloomington, IN<br>
+                        <i class="fa fa-exclamation" style="color: #CC0000" aria-hidden="true"></i> &nbsp;&nbsp;Appointment required<br>
+                        <i class="fa fa-exclamation" style="color: #CC0000" aria-hidden="true"></i> Referral required<br>
+                        <i class="fa fa-exclamation" style="color: #CC0000" aria-hidden="true"></i> &nbsp;&nbsp;Tests limited to certain patients<br>
+                        <i class="fa fa-check" style="color: green" aria-hidden="true"></i> Drive-through<br>
+                        <i class="fa fa-phone info" style="color: #31708f" aria-hidden="true"></i> &nbsp;Call: 777-777-7777
+                    </p>
+                </div>
+            </div>
+            <a href="#" class="primary-btn text-uppercase mx-auto center">Load More Places</a>
+        </div>
+    </div>
+</section>
+<!-- End popular-courses Area -->
+
+<!-- Start search-course Area -->
+<section class="search-course-area relative">
+    <div class="overlay overlay-bg"></div>
+    <div class="container">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-lg-6 col-md-6 search-course-left">
+                <h1 class="text-white">
+                    Can't find one place near you?
+                </h1>
+                <p style="font-size: 16px">
+                    Please enter your zip code to find your nearest testing center.
+                </p>
+                <div class="row details-content">
+                    <div class="col single-detials">
+                    
+                    </div>
+                    <div class="col single-detials">
+                    
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 search-course-right section-gap">
+                <form class="form-wrap" action="">
+                    <h4 class="text-white pb-20 text-center mb-30">Search for COVID-19 Testing Center</h4>
+                    <input type="text" class="form-control" name="zipcode" placeholder="Your Zipcode">
+                    <button class="primary-btn text-uppercase">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End search-course Area -->
+
+<!-- start footer Area -->
+<?php
+    require_once("footer.php");
+?>
+<!-- End footer Area -->
+
+
+<script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.assets/js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="assets/js/vendor/bootstrap.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+<script src="assets/js/easing.min.js"></script>
+<script src="assets/js/hoverIntent.js"></script>
+<script src="assets/js/superfish.min.js"></script>
+<script src="assets/js/jquery.ajaxchimp.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/jquery.tabs.min.js"></script>
+<script src="assets/js/jquery.nice-select.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/mail-script.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
