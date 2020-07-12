829981bbb1c0cd9afc0f2db84a6836d22defcba2
Rocio Cardona <rocio.cardona03@gmail.com>
2020-07-12 15:25:56 -0600

    Created footer.php to simply footer changes and consistency throughout website

---
+++ footer.php
@@ -0,0 +1,74 @@
+<!-- start footer Area -->
+<footer class="footer-area section-gap">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                    <h4></h4>
+                    <ul>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                    <h4>TechPoint University</h4>
+                    <ul>
+                        <li><a href="">Academic Schools</a></li>
+                        <li><a href="">Visit us</a></li>
+                        <li><a href="">Contact us</a></li>
+                        <li><a href="">Public Information</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                    <h4>Our Facilities</h4>
+                    <ul>
+                        <li><a href="">Library</a></li>
+                        <li><a href="">Conferences</a></li>
+                        <li><a href="">IT Support</a></li>
+                        <li><a href="">Research Equipment</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                    <h4>Connect with us</h4>
+                    <ul>
+                        <li><a href="">New students</a></li>
+                        <li><a href="">Alumni</a></li>
+                        <li><a href="">Blogs</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                    <h4>Resources</h4>
+                    <ul>
+                        <li><a href="">Student</a></li>
+                        <li><a href="">Faculty & Staff</a></li>
+                        <li><a href="">Alumni</a></li>
+                        <li><a href="">Community</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom row align-items-center justify-content-between">
+            <p class="footer-text m-0 col-lg-6 col-md-12"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+                All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+            <div class="col-lg-6 col-sm-12 footer-social">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-dribbble"></i></a>
+                <a href="#"><i class="fa fa-behance"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- End footer Area -->
