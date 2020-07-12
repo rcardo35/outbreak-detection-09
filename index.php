9bfa131ce0918f61e8eaf32f070937f13c2fec9e
hemanthrg <hemanthrg@gmail.com>
2020-07-11 19:58:55 -0500

    Review before merging

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 <head>
     <!-- Mobile Specific Meta -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Favicon-->
-    <link rel="shortcut icon" href="assets/img/fav.png">
     <!-- Author Meta -->
     <meta name="author" content="colorlib">
     <!-- Meta Description -->
@@ -64,39 +62,39 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-4">
-                <div class="single-feature">
+                <div class="single-feature" onclick="location.href='self-reporting.php';">
                     <div class="title">
                         <h4>Employee/Staff Self-Reporting</h4>
                     </div>
                     <div class="desc-wrap">
                         <p>
-                            If you are an employee/staff member, please use this page to self-report.
+                            If you are an employee/staff member, please use this page to self-report
                         </p>
-                        <a href="#">Click Here</a>
+                        <a href="self-reporting.php">Click Here</a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4">
-                <div class="single-feature">
+                <div class="single-feature" onclick="location.href='self-reporting.php';">
                     <div class="title">
                         <h4>Student Self-Reporting</h4>
                     </div>
                     <div class="desc-wrap">
                         <p>
-                            If you are a university student, please use this page to self-report.
+                            If you are a university student, please use this page to self-report
                         </p>
                         <a href="#">Click Here</a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4">
-                <div class="single-feature">
+                <div class="single-feature" onclick="location.href='campus-COVID-cases.php';">
                     <div class="title">
                         <h4>Campus COVID Cases</h4>
                     </div>
                     <div class="desc-wrap">
                         <p>
-                            Keep yourself updated about COVID cases in your campus.
+                            Keep yourself updated about COVID cases in your campus
                         </p>
                         <a href="#">Click Here</a>
                     </div>
@@ -106,6 +104,71 @@
     </div>
 </section>
 <!-- End feature Area -->
+
+<!-- Start footer Area -->
+<footer class="footer-area section-gap">
+<div class="container">
+<div class="row">
+<div class="col-lg-3 col-md-6 col-sm-6">
+<div class="single-footer-widget">
+<h4>Top Products</h4>
+<ul>
+<li><a href="#">Managed Website</a></li>
+<li><a href="#">Manage Reputation</a></li>
+<li><a href="#">Power Tools</a></li>
+<li><a href="#">Marketing Service</a></li>
+</ul>
+</div>
+</div>
+<div class="col-lg-3 col-md-6 col-sm-6">
+<div class="single-footer-widget">
+<h4>Quick links</h4>
+<ul>
+<li><a href="#">Jobs</a></li>
+<li><a href="#">Brand Assets</a></li>
+<li><a href="#">Investor Relations</a></li>
+<li><a href="#">Terms of Service</a></li>
+</ul>
+</div>
+</div>
+<div class="col-lg-3 col-md-6 col-sm-6">
+<div class="single-footer-widget">
+<h4>Features</h4>
+<ul>
+<li><a href="#">Jobs</a></li>
+<li><a href="#">Brand Assets</a></li>
+<li><a href="#">Investor Relations</a></li>
+<li><a href="#">Terms of Service</a></li>
+</ul>
+</div>
+</div>
+<div class="col-lg-3 col-md-6 col-sm-6">
+<div class="single-footer-widget">
+<h4>Resources</h4>
+<ul>
+<li><a href="#">Guides</a></li>
+<li><a href="#">Research</a></li>
+<li><a href="#">Experts</a></li>
+<li><a href="#">Agencies</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="footer-bottom row align-items-center justify-content-between">
+<p class="footer-text m-0 col-lg-6 col-md-12"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+<div class="col-lg-6 col-sm-12 footer-social">
+<a href="#"><i class="fa fa-facebook"></i></a>
+<a href="#"><i class="fa fa-twitter"></i></a>
+<a href="#"><i class="fa fa-dribbble"></i></a>
+<a href="#"><i class="fa fa-behance"></i></a>
+</div>
+</div>
+</div>
+</footer>
+<!-- End footer Area -->
 
 <script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.assets/js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
