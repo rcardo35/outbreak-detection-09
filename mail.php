256dcb0b4b7a577a4fab5996f62aba210e3c0d28
Rocio Cardona <rocio.cardona03@gmail.com>
2020-07-17 12:58:00 -0600

    Testing alternative mailing

--- mail.php
+++ mail.php
@@ -1,52 +1,58 @@
 <?php
-////    require 'vendor/autoload.php'; // If you're using Composer (recommended)
-//// Comment out the above line if not using Composer
+//    require 'vendor/autoload.php'; // If you're using Composer (recommended)
+// Comment out the above line if not using Composer
 //    require("sendgrid-php/sendgrid-php.php");
-//// If not using Composer, uncomment the above line and
-//// download sendgrid-php.zip from the latest release here,
-//// replacing <PATH TO> with the path to the sendgrid-php.php file,
-//// which is included in the download:
-//// https://github.com/sendgrid/sendgrid-php/releases
-//
-//
-//        $to = filter_var($_POST['demo-email'], FILTER_SANITIZE_EMAIL);
-//        $building = filter_var($_POST['building'], FILTER_SANITIZE_STRING);
-//
-//        $email = new \SendGrid\Mail\Mail();
-//        $email->setFrom("covidalert@techpointuniversity.edu", "TechPoint University Alert System");
-//        $email->setSubject("COVID-19 Alert");
-//        $email->addTo('rcardona4@miners.utep.edu', "University Member");
-//        $email->addContent("text/html", "<strong>Univeristy Alert:</strong>
-//            <p>A University team member who was in the <b>building</b> building within the last two weeks has been confirmed with COVID-19 and is under medical supervision off-campus. Until further notice no one is permitted to enter the <b>building</b> while the University employs professional sanitation and protocols for restoring the area for entry and use.</p>
-//            <p>This is an automated notification, please do not reply to this email. If you have any questions please contact: <mailto>healthcenter@techpointuniversity.edu directly.</mailto></p>"
-//        );
-//        $sendgrid = new \SendGrid('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');
-//        try {
-//            $response = $sendgrid->send($email);
-//            print $response->statusCode() . "\n";
-//            print_r($response->headers());
-//            print $response->body() . "\n";
-//        } catch (Exception $e) {
-//            echo 'Caught exception: ' . $e->getMessage() . "\n";
-//        }
+// If not using Composer, uncomment the above line and
+// download sendgrid-php.zip from the latest release here,
+// replacing <PATH TO> with the path to the sendgrid-php.php file,
+// which is included in the download:
+// https://github.com/sendgrid/sendgrid-php/releases
+    
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+    
+    require 'PHPMailer-master/src/Exception.php';
+    require 'PHPMailer-master/src/PHPMailer.php';
+    require 'PHPMailer-master/src/SMTP.php';
+// Load Composer's autoloader
+//    require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+    
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.sendgrid.net';                    // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'apikey';                     // SMTP username
+        $mail->Password   = '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';                               // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    
+        //Recipients
+        $mail->setFrom('from@example.com', 'Mailer');
+        $mail->addAddress('rcardona4@miners.utep.edu', 'Joe User');     // Add a recipient
+//        $mail->addAddress('ellen@example.com');               // Name is optional
+//        $mail->addReplyTo('info@example.com', 'Information');
+//        $mail->addCC('cc@example.com');
+//        $mail->addBCC('bcc@example.com');
     
     
-    $client  = new http\Client;
-    $request = new http\Client\Request;
-    $request->setRequestUrl('https://aquamarine-falcon-3956.twil.io/sendEmail');
-    $request->setRequestMethod('POST');
-    $body = new http\Message\Body;
-    $body->append('{
-    "TO": "rcardona4@miners.utep.edu",
-    "FROM": "sms@bdm.owldemo.com",
-    "SUBJECT": "testing",
-    "BODY": "Hello World"
-}');
-    $request->setBody($body);
-    $request->setOptions(array());
-    $request->setHeaders(array(
-            'Content-Type' => 'application/json'
-    ));
-    $client->enqueue($request)->send();
-    $response = $client->getResponse();
-    echo $response->getBody();
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    
+        $mail->send();
+        echo 'Message has been sent';
+        header('Location: index.php');
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
