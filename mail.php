0d99aff0525c17cdb4fa40629b6623fe64ca89d9
Rocio Cardona <rocio.cardona03@gmail.com>
2020-07-17 14:57:18 -0600

    Mailer script works

--- mail.php
+++ mail.php
@@ -1,13 +1,4 @@
 <?php
-//    require 'vendor/autoload.php'; // If you're using Composer (recommended)
-// Comment out the above line if not using Composer
-//    require("sendgrid-php/sendgrid-php.php");
-// If not using Composer, uncomment the above line and
-// download sendgrid-php.zip from the latest release here,
-// replacing <PATH TO> with the path to the sendgrid-php.php file,
-// which is included in the download:
-// https://github.com/sendgrid/sendgrid-php/releases
-    
 
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
@@ -18,11 +9,12 @@
     require 'PHPMailer-master/src/Exception.php';
     require 'PHPMailer-master/src/PHPMailer.php';
     require 'PHPMailer-master/src/SMTP.php';
-// Load Composer's autoloader
-//    require 'vendor/autoload.php';
 
 // Instantiation and passing `true` enables exceptions
     $mail = new PHPMailer(true);
+    
+    $to = $_POST['demo-email'];
+    $building = $_POST['building'];
     
     try {
         //Server settings
@@ -31,32 +23,28 @@
         $mail->Host       = 'smtp.sendgrid.net';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
         $mail->Username   = 'apikey';                     // SMTP username
-        $mail->Password   = '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';                               // SMTP password
+        $mail->Password   = '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';                               // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     
         //Recipients
-        $mail->setFrom('from@example.com', 'Mailer');
-        $mail->addAddress('rcardona4@miners.utep.edu', 'Joe User');     // Add a recipient
-//        $mail->addAddress('ellen@example.com');               // Name is optional
-//        $mail->addReplyTo('info@example.com', 'Information');
-//        $mail->addCC('cc@example.com');
-//        $mail->addBCC('bcc@example.com');
+        $mail->setFrom('covidalert@techpointuniversity.edu', 'TechPoint University COVID-19 Alert System');
+        $mail->addAddress($to);     // Add a recipient
+        $mail->addCC('rcardona4@miners.utep.edu');
     
     
         // Content
         $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = '<p><b>Univeristy Alert:</b></p>
-                          <p>A University team member who was in Library within the last two weeks has been confirmed with COVID-19 and is under medical supervision off-campus. Until further notice no one is permitted to enter Library while the University employs professional sanitation and protocols for restoring the area for entry and use.</p>
-                          <p>This is an automated notification, please do not reply to this email. If you have any questions please contact: healthcenter@techpointuniversity.edu directly.
+        $mail->Subject = 'COVID-19 ALERT';
+        $mail->Body    = "<p><b>University Alert:</b></p>
+                          <p>A University team member who was in the {$building} building within the last two weeks has been confirmed with COVID-19 and is under medical supervision off-campus. Until further notice no one is permitted to enter the {$building} building while the University employs professional sanitation and protocols for restoring the area for entry and use.</p>
+                          <p>This is an automated notification, please do not reply to this email.<br> If you have any questions please contact: healthcenter@techpointuniversity.edu directly.
 
-</p>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+</p>";
     
         $mail->send();
         echo 'Message has been sent';
-//        header('Location: index.php');
+        header('Location: health-center-view.php?noaccess=false');
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
