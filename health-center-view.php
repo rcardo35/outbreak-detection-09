0ad23ab073c78ab7fcc6bddb6ee846ceb220c798
Rocio Cardona <rocio.cardona03@gmail.com>
2020-07-18 16:37:35 -0600

    testing fix #5

--- health-center-view.php
+++ health-center-view.php
@@ -141,6 +141,13 @@
             color: #C00;
             font-size: 12px;
         }
+
+        .quotes {
+            margin-top: 20px;
+            padding: 30px;
+            background-color: rgb(255, 255, 255);
+            box-shadow: -20.84px 21.58px 30px 0px rgba(176, 176, 176, 0.1);
+        }
     </style>
 </head>
 <body>
@@ -264,20 +271,6 @@
                     <td></td>
                 </tr>
                 </tbody>
-                <tfoot>
-                <tr>
-                    <th>Case ID</th>
-                    <th>Name</th>
-                    <th>Phone Number</th>
-                    <th>Email</th>
-                    <th>Department</th>
-                    <th>Campus Places</th>
-                    <th>Symptomatic</th>
-                    <th>Medically Confirmed</th>
-                    <th>Confirmed Date</th>
-                    <th>Edit</th>
-                </tr>
-                </tfoot>
             </table>
         </div>
 
@@ -315,119 +308,42 @@
                     <td></td>
                 </tr>
                 </tbody>
-                <tfoot>
-                <tr>
-                    <th>Case ID</th>
-                    <th>Name</th>
-                    <th>Phone Number</th>
-                    <th>Email</th>
-                    <th>Department</th>
-                    <th>Campus Places</th>
-                    <th>Symptomatic</th>
-                    <th>Medically Confirmed</th>
-                    <th>Confirmed Date</th>
-                    <th>Edit</th>
-                </tr>
-                </tfoot>
             </table>
         </div>
+        <div class="row justify-content-between">
+            <div class="col-6">
+            </div>
+            <div class="col-6">
+                <div class="quotes">
+                    <a href="send-alert.php" target="_blank"><h4 class="mt-20 mb-20">Test our email alert system!</h4></a>
+                    <p>
+                        This email alert will allow health center staff to send out email alerts when a new case is confirmed<br>
+                        <span style="float:right;"><a href="send-alert.php" target="_blank">Learn more! <i class="fa fa-arrow-right" aria-hidden="true"></i></a></span>
+                    </p>
+                </div>
+            </div>
+        </div>
+
     </div>
 </section>
 <!-- End contact-page Area -->
 
 <!-- start footer Area -->
-<footer class="footer-area section-gap">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-2 col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-                    <h4>Top Products</h4>
-                    <ul>
-                        <li><a href="#">Managed Website</a></li>
-                        <li><a href="#">Manage Reputation</a></li>
-                        <li><a href="#">Power Tools</a></li>
-                        <li><a href="#">Marketing Service</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-2 col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-                    <h4>Quick links</h4>
-                    <ul>
-                        <li><a href="#">Jobs</a></li>
-                        <li><a href="#">Brand Assets</a></li>
-                        <li><a href="#">Investor Relations</a></li>
-                        <li><a href="#">Terms of Service</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-2 col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-                    <h4>Features</h4>
-                    <ul>
-                        <li><a href="#">Jobs</a></li>
-                        <li><a href="#">Brand Assets</a></li>
-                        <li><a href="#">Investor Relations</a></li>
-                        <li><a href="#">Terms of Service</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-2 col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-                    <h4>Resources</h4>
-                    <ul>
-                        <li><a href="#">Guides</a></li>
-                        <li><a href="#">Research</a></li>
-                        <li><a href="#">Experts</a></li>
-                        <li><a href="#">Agencies</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-4  col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-                    <h4>Newsletter</h4>
-                    <p>Stay update with our latest</p>
-                    <div class="" id="mc_embed_signup">
-                        <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get">
-                            <div class="input-group">
-                                <input type="text" class="form-control" name="EMAIL" placeholder="Enter Email Address" onfocus="this.placeholder = ''"
-                                       onblur="this.placeholder = 'Enter Email Address '" required="" type="email">
-                                <div class="input-group-btn">
-                                    <button class="btn btn-default" type="submit">
-                                        <span class="lnr lnr-arrow-right"></span>
-                                    </button>
-                                </div>
-                                <div class="info"></div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="footer-bottom row align-items-center justify-content-between">
-            <p class="footer-text m-0 col-lg-6 col-md-12"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                Copyright &copy;<script>document.write(new Date().getFullYear());</script>
-                All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-            <div class="col-lg-6 col-sm-12 footer-social">
-                <a href="#"><i class="fa fa-facebook"></i></a>
-                <a href="#"><i class="fa fa-twitter"></i></a>
-                <a href="#"><i class="fa fa-dribbble"></i></a>
-                <a href="#"><i class="fa fa-behance"></i></a>
-            </div>
-        </div>
-    </div>
-</footer>
+<?php
+    require_once("footer.php");
+?>
 <!-- End footer Area -->
 
 <!--Edit Modal-->
 <div class="modal fade" id="editData" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered" role="document">
         <div class="modal-content">
-            <form action="#" method="post" id="updateForm">
+            <form action="" method="post" id="updateForm">
                 <div class="modal-header">
                     <h4 class="modal-title" id="myModalLabel">Update Entry</h4>
-                    <button type="button" class="close req" data-dismiss="modal" aria-label="Close"><spanaria-hidden="true">&times;</span></button>
+                    <button type="button" class="close req" data-dismiss="modal" aria-label="Close">
+                        <spanaria-hidden
+                        ="true">&times;</span></button>
                 </div>
                 <div class="modal-body" id="info_update">
                 </div>
@@ -456,7 +372,7 @@
 <script src="assets/js/jquery.nice-select.min.js"></script>
 <script src="assets/js/owl.carousel.min.js"></script>
 <script src="assets/js/main.js"></script>
-<script src="assets/js/vendor/bootstrap.min.js" type="text/javascript"></script>
+<!--<script src="assets/js/vendor/bootstrap.min.js" type="text/javascript"></script>-->
 
 <!--<script src="https://code.jquery.com/jquery-3.5.1.js" type="text/javascript"></script>-->
 <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" type="text/javascript"></script>
--- mail.php
+++ mail.php
@@ -16,35 +16,36 @@
     $to = $_POST['demo-email'];
     $building = $_POST['building'];
     
-    try {
-        //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'smtp.sendgrid.net';                    // Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'apikey';                     // SMTP username
-        $mail->Password   = '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';                               // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     
-        //Recipients
-        $mail->setFrom('covidalert@techpointuniversity.edu', 'TechPoint University COVID-19 Alert System');
-        $mail->addAddress($to);     // Add a recipient
-        $mail->addCC('rcardona4@miners.utep.edu');
-    
-    
-        // Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'COVID-19 ALERT';
-        $mail->Body    = "<p><b>University Alert:</b></p>
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.sendgrid.net';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'apikey';                     // SMTP username
+            $mail->Password   = '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';                               // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        
+            //Recipients
+            $mail->setFrom('covidalert@techpointuniversity.edu', 'TechPoint University COVID-19 Alert System');
+            $mail->addAddress($to);     // Add a recipient
+            $mail->addCC('rcardona4@miners.utep.edu');
+        
+        
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'COVID-19 ALERT';
+            $mail->Body    = "<p><b>University Alert:</b></p>
                           <p>A University team member who was in the <b>{$building}</b> within the last two weeks has been confirmed with COVID-19 and is under medical supervision off-campus. Until further notice no one is permitted to enter the <b>{$building}</b> while the University employs professional sanitation and protocols for restoring the area for entry and use.</p>
                           <p>This is an automated notification, please do not reply to this email.<br> If you have any questions please contact: healthcenter@techpointuniversity.edu directly.
 
 </p>";
-    
-        $mail->send();
-        echo 'Message has been sent';
-        header('Location: health-center-view.php?noaccess=false');
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        
+            $mail->send();
+            echo 'Message has been sent';
+            header('Location: health-center-view.php?noaccess=false');
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
