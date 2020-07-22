2a7fa71116687a9e0daa55dda2b4229273bba5ea
Rocio Cardona <rocio.cardona03@gmail.com>
2020-07-21 22:29:26 -0600

    testing database using getenv

--- config/database_config.php
+++ config/database_config.php
@@ -1,15 +1,16 @@
 <?php
-    $host = "us-cdbr-east-02.cleardb.com";
-    $db = "heroku_c224005b36bf596";
-    $user = "ba1b3bebb866bc";
-    $password ="bc392e11";
-    $charset = "";
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
     
     global $conn;
     $dsn = "mysql:host=$host;dbname=$db";
     
     try {
-        $conn = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+        $conn = new PDO("mysql:host=$host;dbname=$db", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (\PDOException $e){
         throw new \PDOException($e->getMessage(), (int)$e->getCode());
